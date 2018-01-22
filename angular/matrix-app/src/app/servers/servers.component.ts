70cba15083d676bf3395f3529284c204fbef482b
Pubudu  Dissanayake <u5857495@anu.edu.au>
2018-01-22 14:47:44 +1100

    use of selector meta-data tag in different senarios

--- angular/matrix-app/src/app/servers/servers.component.ts
+++ angular/matrix-app/src/app/servers/servers.component.ts
@@ -1,8 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
+  // default selector
   selector: 'app-servers',
-  templateUrl: './servers.component.html',
+  // selector by attribute
+  // selector:'[app-servers]',
+
+  // selector by class
+  // selector:'.app-servers',
+
+  // use html code directly as template
+  template: `
+  <app-server></app-server>
+  <app-server></app-server>`,
+  // defualt
+  // templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
