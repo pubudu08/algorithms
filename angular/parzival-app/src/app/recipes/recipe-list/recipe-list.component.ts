c8064aa7f7172c2c26cf0cff16fe93f6347d9406
Pubudu  Dissanayake <u5857495@anu.edu.au>
2018-01-25 19:00:59 +1100

    Output Recipe object details with ngFor

--- angular/parzival-app/src/app/recipes/recipe-list/recipe-list.component.ts
+++ angular/parzival-app/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -11,7 +11,9 @@ export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
     new Recipe('A Test Recipe','This is a test recipe',
     'https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2013/05/frying-pan-pizza-easy-recipe-collection.jpg')
-  ];
+  ,new Recipe('A Test Recipe','This is a test recipe',
+  'https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2013/05/frying-pan-pizza-easy-recipe-collection.jpg')
+];
   constructor() { }
 
   ngOnInit() {
