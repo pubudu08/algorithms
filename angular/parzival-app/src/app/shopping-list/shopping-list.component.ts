abe8673b6d788fe0573b5ab29884ee4430e5cc7c
Pubudu  Dissanayake <u5857495@anu.edu.au>
2018-02-06 00:21:26 +1100

    Used Service to push Notifications

--- angular/parzival-app/src/app/shopping-list/shopping-list.component.ts
+++ angular/parzival-app/src/app/shopping-list/shopping-list.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
-import { Ingredient } from '../shared/ingredient.model';
+import {Ingredient} from '../shared/ingredient.model';
 import {ShoppingListService} from './shopping-list.service';
 
 @Component({
@@ -11,9 +11,13 @@ import {ShoppingListService} from './shopping-list.service';
 export class ShoppingListComponent implements OnInit {
   ingredients: Ingredient[];
 
-  constructor(private shoppingListService: ShoppingListService) { }
+  constructor(private shoppingListService: ShoppingListService) {
+  }
 
   ngOnInit() {
     this.ingredients = this.shoppingListService.ingredients;
+    this.shoppingListService.ingredientsChanged.subscribe(
+      (ingredients: Ingredient[]) =>
+        this.ingredients = ingredients);
   }
 }
--- angular/parzival-app/src/app/shopping-list/shopping-list.service.ts
+++ angular/parzival-app/src/app/shopping-list/shopping-list.service.ts
@@ -1,6 +1,9 @@
 import {Ingredient} from '../shared/ingredient.model';
+import {EventEmitter} from '@angular/core';
 
 export class ShoppingListService {
+
+  ingredientsChanged = new EventEmitter<Ingredient[]>();
   private _ingredients: Ingredient[] = [
     new Ingredient('Apples', 5),
     new Ingredient('Tomatoes', 10),
@@ -8,11 +11,12 @@ export class ShoppingListService {
 
 
   get ingredients(): Ingredient[] {
-    return this._ingredients;
+    return this._ingredients.slice();
   }
 
   addIngredient(ingredient: Ingredient) {
     this._ingredients.push(ingredient);
+    this.ingredientsChanged.emit(this._ingredients.slice());
 
   }
 }
