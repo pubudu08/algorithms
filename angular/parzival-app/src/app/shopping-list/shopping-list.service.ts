f32b62f054cec72ad392f2d5faf5be2e80392331
Pubudu  Dissanayake <u5857495@anu.edu.au>
2018-02-06 00:13:27 +1100

    Added ShoppingListService and removed EventEmitters

--- angular/parzival-app/src/app/shopping-list/shopping-list.service.ts
+++ angular/parzival-app/src/app/shopping-list/shopping-list.service.ts
@@ -1,3 +1,18 @@
-export class ShoppingListService {
+import {Ingredient} from '../shared/ingredient.model';
 
+export class ShoppingListService {
+  private _ingredients: Ingredient[] = [
+    new Ingredient('Apples', 5),
+    new Ingredient('Tomatoes', 10),
+  ];
+
+
+  get ingredients(): Ingredient[] {
+    return this._ingredients;
+  }
+
+  addIngredient(ingredient: Ingredient) {
+    this._ingredients.push(ingredient);
+
+  }
 }
